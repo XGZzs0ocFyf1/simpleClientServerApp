fe6b647d01254fe4d016015baad912f859e3a0dc
XGZzs0ocFyf1 <none>
2021-02-15 01:17:34 +1000

    Add client and server classes for simple socket-based chat.

---
+++ src/client/Client.java
@@ -0,0 +1,104 @@
+package client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Client {
+
+
+    private final int SERVER_PORT = 8082;
+    private final String SERVER_ADDRESS = "localhost";
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private JTextArea chatArea;
+    private JTextField msgInputField;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+
+    public Client() {
+        try {
+            connection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        prepareGUI();
+    }
+
+    public void connection() throws IOException {
+        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        dis = new DataInputStream(socket.getInputStream());
+        dos = new DataOutputStream(socket.getOutputStream());
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    String message = dis.readUTF();
+                    if (message.equalsIgnoreCase("/stop")) {
+                        System.out.println("Client  stopped by outer command /stop");
+                        break;
+                    }
+                    var time = formatter.format(LocalDateTime.now());
+                    chatArea.append("Server [" + time + "] : " + message + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+
+    public void send() {
+        if (msgInputField.getText() != null && !msgInputField.getText().trim().isEmpty()) {
+            try {
+                var time = formatter.format(LocalDateTime.now());
+                dos.writeUTF(msgInputField.getText());
+                chatArea.append("Client [" + time + "] : " + msgInputField.getText() + "\n");
+                msgInputField.setText("");
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    private void prepareGUI() {
+        JFrame frame = new JFrame("Chat client");
+        frame.setBounds(600, 300, 400, 400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        BorderLayout borderLayout = new BorderLayout();
+        frame.setLayout(borderLayout);
+        frame.setResizable(false);
+        chatArea = new JTextArea(21, 20);
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        chatArea.setBackground(new Color(190, 134, 243));
+
+        msgInputField = new JTextField(29);
+        var sendButton = new JButton("send");
+
+        sendButton.addActionListener(e -> {
+            send();
+        });
+
+        msgInputField.addActionListener(e -> {
+            send();
+        });
+
+        frame.add(new JScrollPane(chatArea), BorderLayout.NORTH);
+        frame.add(msgInputField, BorderLayout.WEST);
+        frame.add(sendButton, BorderLayout.EAST);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            new Client();
+        });
+    }
+}
---
+++ src/server/Server4.java
@@ -0,0 +1,77 @@
+package server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Server4 {
+
+    private static final int PORT = 8082;
+
+
+    private static void startServer() {
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+
+            System.out.printf("Server started on port %s\n", PORT);
+
+            var socket = serverSocket.accept();
+
+            System.out.printf("Client connected to port %s\n", PORT);
+            var dis = new DataInputStream(socket.getInputStream());
+            var dos = new DataOutputStream(socket.getOutputStream());
+
+            //this thread handles messages from client
+            Thread clientMessagePrinterThread = new Thread(() -> {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+                System.out.println("Thread for handling client messages started.");
+                try {
+                    while (true) {
+                        var message = dis.readUTF();
+                        if (message.equalsIgnoreCase("/stop")) {
+                            System.out.println("Thread stopped by outer command /stop");
+                            break;
+                        }
+                        var time = formatter.format(LocalDateTime.now());
+                        System.out.printf("Client [%s] : %s \n", time, message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            });
+
+
+
+            //this thread handles server messages for client
+            Thread serverMessageInputThread = new Thread(() -> {
+                System.out.println("Server message input thread started.");
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+                    while (true) {
+                        var message = br.readLine();
+                        dos.writeUTF(message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            });
+
+            clientMessagePrinterThread.start();
+            serverMessageInputThread.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        startServer();
+    }
+}
